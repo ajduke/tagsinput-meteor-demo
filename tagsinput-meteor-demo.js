9642e1f47691914e6444e8d9b157cbb1188c2d02
ajduke <ajduke@about.me>
2015-06-27 14:42:49 +0530

    updated example for typeahead

--- tagsinput-meteor-demo.js
+++ tagsinput-meteor-demo.js
@@ -2,6 +2,36 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
 
+  Template.hello.rendered= function(){
+    console.log('rednedered here ')
+    var citynames = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: [{name:"one"}, {name:"two"}, {name:"three"}]
+    });
+    citynames.initialize();
+
+    $('.suggest').tagsinput({
+      typeaheadjs: {
+        name: 'citynames',
+        displayKey: 'name',
+        valueKey: 'name',
+        source: citynames.ttAdapter()
+      }
+    });
+
+    //$('#prefetch .typeahead').typeahead(null, {
+    //  // `ttAdapter` wraps the suggestion engine in an adapter that
+    //  // is compatible with the typeahead jQuery plugin
+    //  name: 'states',
+    //  source: function(){
+    //    return ['Mumbai', 'Amsterdam', 'Paris']
+    //  }
+    //
+    //});
+  }
+
+
   Template.hello.helpers({
     counter: function () {
       return Session.get("counter");
